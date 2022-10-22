cfe2552e7de05a2d24f88753fa37679752ef166d
PsclDev <me@pscl.dev>
2022-10-23 00:13:45 +0200

    feat: clipboard globalProperty to hook

---
+++ src/modules/common/hooks/useClipboard.ts
@@ -0,0 +1,18 @@
+import { info as InfoNotification } from '@/components/common/NotificationPlugin';
+
+export default function useClipboard() {
+  const copyTo = (value: string, message = 'Copied to clipboard') => {
+    const el = document.createElement('textarea');
+    el.value = value;
+
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    InfoNotification(message);
+  };
+
+  return {
+    copyTo,
+  };
+}
--- src/plugins/globalPlugins.ts
+++ src/plugins/globalPlugins.ts
@@ -1,10 +1,8 @@
 import { App } from 'vue';
-import copyToClipboard from '@/plugins/copyToClipboard';
 import NotificationsPlugin from '@/components/common/NotificationPlugin';
 
 export default {
   install(Vue: App) {
-    Vue.use(copyToClipboard);
     Vue.use(NotificationsPlugin);
   },
 };
